16f908a0764cc86db9c79eecc0f3abfb66e41830
Sidney <sidney.yim@gmail.com>
2010-10-01 04:48:13 +0800

    RC2

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
-<<<<<<< HEAD
-Rails::Initializer.run do |config|
-=======
->>>>>>> BBB
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
